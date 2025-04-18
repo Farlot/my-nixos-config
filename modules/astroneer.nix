210dc48ea427dbd67d5f17c4765b95202e4ee37b
Farlot <m.waaagan@gmail.com>
2025-04-18 05:05:02 +0200

    added astroneer for some reason

---
+++ modules/astroneer.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.oci-containers.containers.soulmask = {
+    # Url : https://hub.docker.com/r/kagurazakanyaa/soulmask
+    image = "docker.io/kagurazakanyaa/soulmask";
+    ports = [ "8777:8777/udp" ]; # Adjust as needed
+    volumes = [
+      "/mnt/stuff/DockerStuff/soulmask:/opt/soulmask/WS/Saved" # Map a local directory for persistent data
+    ];
+    #cmd =[ "" ];
+  };
+}
