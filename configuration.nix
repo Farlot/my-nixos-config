839b43ccdd4e682048e780c957493622dc1567ac
Farlot <m.waaagan@gmail.com>
2025-05-05 22:52:32 +0200

    adding factorio

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./modules/maintenance.nix
+      ./modules/factorio.nix
       #./modules/avorion.nix
       #./modules/soulmask.nix
       inputs.home-manager.nixosModules.home-manager
