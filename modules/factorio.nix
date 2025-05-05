6fc8eaf66d1e746608c187545f0ef20e1d4e7c50
Farlot <m.waaagan@gmail.com>
2025-05-06 01:41:10 +0200

    factorio added and working

--- modules/factorio.nix
+++ modules/factorio.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.oci-containers.containers.avorion = {
-    image = "docker.io/factoriotools/factorio/";
+  virtualisation.oci-containers.containers.factorio = {
+    image = "docker.io/factoriotools/factorio";
     ports = [ "34197:34197/udp" "27015:27015/tcp" ]; # Adjust as needed
     volumes = [
       "/mnt/stuff/DockerStuff/factorio:/factorio" # Map a local directory for persistent data
     ];
+    environment = {
+      UPDATE_MODS_ON_START = "true";
+    };
   };
 }
