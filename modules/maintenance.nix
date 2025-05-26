23bafef566a58daa74eabd107d8a6ae9d97dd2c0
Farlot <m.waaagan@gmail.com>
2025-05-26 10:32:15 +0200

    Finally working autoupdate with git

--- modules/maintenance.nix
+++ modules/maintenance.nix
@@ -1,6 +1,16 @@
 { config, pkgs, inputs, ... }:
 
 {
+    systemd.services.nixos-upgrade = {
+        serviceConfig = {
+            Environment = [
+                "GIT_AUTHOR_NAME=NixOS Automated Upgrade"
+                "GIT_AUTHOR_EMAIL=root@riggen.local"
+                "GIT_COMMITTER_NAME=NixOS Automated Upgrade"
+                "GIT_COMMITTER_EMAIL=root@riggen.local"
+            ];
+        };
+    };
     # Automatic updates
     system.autoUpgrade = {
         enable = true;
