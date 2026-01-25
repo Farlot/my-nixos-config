1ec0aa4bceb77b488505f098d61e5cfe749c6cfa
Farlot <m.waaagan@gmail.com>
2026-01-25 23:11:21 +0100

    tidy unused

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -8,10 +8,6 @@
       kde.enable = true;
       gtk.enable = true;
     };
-    # image = pkgs.fetchurl {
-    #   url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/master/wallpapers/nix-wallpaper-nineish-dark-gray.png";
-    #   sha256 = "07zl1dlxqh9dav9pibnhr2x1llywwnyphmzcdqaby7dz5js184ly";
-    # };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     polarity = "dark";
 
